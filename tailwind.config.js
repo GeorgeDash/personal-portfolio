5d951f3adab3e8451df393ddb476bc07f8652f74
GeorgeDash <georgi.milen.zhelev@gmail.com>
2024-09-23 09:19:44 +0300

    In progress

---
+++ tailwind.config.js
@@ -0,0 +1,71 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        "sm": "320px",
+        "sm-m": "375px",
+        "sm-l": "425px",
+      },
+      fontSize: {
+        md: ['1.063rem', '1.5rem']
+      },
+      lineHeight: {
+        "11": "1.313rem"
+      },
+      backgroundImage:{
+        'homepage': "url('../img/homepage.jpg')",
+      },
+      fontFamily: {
+        "sans": "'Inter'"
+      },
+      width: {
+        "40-p": "40%",
+        "45-p": "45%",
+        "65-p": "65%",
+        "75-p": "75%",
+        "90-p": "90%",
+        "98-r": "45rem"
+      },
+      spacing: {
+        "15-r": "3.75rem",
+        "20-p": "20%",
+        "25-p": "25%",
+        "40-p": "40%",
+        "50-p": "50%",
+        "80-p": "80%",
+        "90-p": "90%",
+        "95-p": "95%",
+        "99-p": "99%",
+        "100-p": "100%"
+      },
+      transitionDuration: {
+        '375': '375ms'
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': {filter: 'opacity(0)'},
+          '100%': {filter: 'opacity(1)'}
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 1s ease-in-out linear forwards'
+      },
+      colors: {
+        "beagle": "#b49278"
+      },
+      transitionProperty: {
+        'width': 'width'
+      },
+      height: {
+        "2-p": "2px",
+        "50-p": "50%",
+        "75-p": "75%",
+        "51-px": "3.188rem"
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+
